d5785b0890b6b98dbbbc78285ee7c6cb7f8ee862
hakurou-white <hakurou178@gmail.com>
2019-12-23 23:10:12 +0900

    background

--- game.py
+++ game.py
@@ -8,17 +8,30 @@ class Game:
         self.tk.title("Ms Pict Rase for the Exit")
         self.tk.resizable(0,0)
         self.tk.wm_attributes("-topmost",1)
-        self.canvas=Canvas(tk,width=500,height=500,highlightthickness=0)
+        self.canvas=Canvas(self.tk,width=500,height=500,\
+            highlightthickness=0)
         self.canvas.pack()
         self.tk.update()
         self.canvas_height=500
         self.canvas_width=500
-        self.bg=PhotoImage(file="background.gif")
+        self.bg=PhotoImage(file='background.gif')
         w=self.bg.width()
         h=self.bg.height() 
         for i in range(0,5):
             for j in range(0,5):
-                self.canvas.create_image(i*w,j*h,image=self.bg,anchor='nw')
+                self.canvas.create_image(i*w,j*h,\
+                    image=self.bg,anchor='nw')
         self.sprites=[]
-        self.running=True      
+        self.running=True    
+
+    def mainloop(self):
+        while True:
+            if self.running==True:
+                for sprite in self.sprites:
+                    sprite.move()
+            self.tk.update()
+            time.sleep(0.01)
+
+g=Game()
+g.mainloop()
 
