c1cc6d2957d5699c6797556c42a550155ceb0ec3
hakurou-white <hakurou178@gmail.com>
2019-12-25 09:38:09 +0900

    pict

--- game.py
+++ game.py
@@ -103,6 +103,47 @@ class PlatformSprite(Sprite):
         self.image=game.canvas.create_image(x,y,image=self.photo_image,anchor='nw')
         self.coordinates=Coords(x,y,x+width,y+height)
 
+class StickFigureSprite(Sprite):
+    def __init__(self,game):
+        Sprite.__init__(self,game)
+        self.images_left=[
+            PhotoImage(file="figure-L1.gif"),
+            PhotoImage(file="figure-L2.gif"),
+            PhotoImage(file="figure-L3.gif")
+        ]
+        self.images_right=[
+            PhotoImage(file="figure-R1.gif"),
+            PhotoImage(file="figure-R2.gif"),
+            PhotoImage(file="figure-R3.gif")
+        ]
+        self.image=game.canvas.create_image(200,470,image=self.images_left[0],anchor='nw')
+        self.x=-2
+        self.y=0
+        self.current_image=0
+        self.current_image_add=1
+        self.jump_count=0
+        self.last_time=time.time()
+        self.coordinates=Coords()
+        game.canvas.bind_all('<KeyPress-Left>',self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>',self.turn_right)
+        game.canvas.bind_all('<space>',self.jump)
+
+    def turn_left(self,evt):
+        if self.y==0:
+            self.x=-2
+
+    def turn_right(self,evt):
+        if self.y==0:
+            self.x=2
+
+    def jump(self,evt):
+        if self.y==0:
+            self.y=-4
+            self.jump_count=0
+    
+
+
+
 g=Game()
 platform1=PlatformSprite(g,PhotoImage(file="platform1.gif"),0,480,100,10)
 platform2=PlatformSprite(g,PhotoImage(file="platform1.gif"),150,440,100,10)
